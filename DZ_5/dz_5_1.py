fe2e051ff0a44a63f65f8e7f8745548eacb62780
SeregaLi <kaprall13rus@gmail.com>
2023-11-08 09:23:22 +0300

    DZ 5   fin

--- DZ_5/dz_5_1.py
+++ DZ_5/dz_5_1.py
@@ -5,17 +5,10 @@
 file_path = "C:/Users/User/Documents/example.txt"
 
 
-import os
-
-
 def get_file_info(file_path):
-    file_name, file_extension = os.path.splitext(file_path)
-    path = os.path.dirname(file_path)
-    if not path.endswith('/'):
-        path += '/'  # Добавляем символ '/' в конец пути
-    name = os.path.basename(file_name)
-
-    return (path, name, file_extension)
-
+    file_name = file_path.split("/")[-1]
+    file_extension = file_name.split(".")[-1]
+    path = file_path[:-len(file_name)]
+    return (path, file_name[:-len(file_extension)-1], "." + file_extension)
 
 print(get_file_info(file_path="C:/Users/User/Documents/example.txt"))
